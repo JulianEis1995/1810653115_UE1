de635f6740caa522cba2c415b4cac1db2d79387a
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-11 14:58:59 +0100

    Aufgabe3

---
+++ src/Aufgabe3_1810653115.java
@@ -0,0 +1,24 @@
+public class Aufgabe3_1810653115
+{
+    public static void main(String[] args)
+    {
+        //Variablen vom Typ int erstellen und Werte zuweisen
+        int zahl1 = 30;
+        int zahl2 = 20;
+        int zahl3 = 10;
+
+        //Variablen vom Typ double erstellen und Werte zuweisen
+        double zahl4 = 1.5;
+        double zahl5 = 2.5;
+        double zahl6 = 3.5;
+
+        //Zahlen addidieren, dividieren, multiplizieren und Modulo berechen
+        System.out.println(zahl1 + zahl2);
+        System.out.println(zahl1 / zahl3);
+        System.out.println(zahl4 * zahl2);
+        System.out.println(zahl1 % zahl2);
+        //Zahlen inkrementieren und dekrementieren
+        System.out.println(zahl3++);
+        System.out.println(zahl2--);
+    }
+}
