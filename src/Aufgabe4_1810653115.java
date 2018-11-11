7336f2e72f217d8851bf406c46e5c8acbfed2e58
Julian Eisenmann <1810653115@stud.fh-kufstein.ac.at#>
2018-11-11 15:04:54 +0100

    Aufgabe4

---
+++ src/Aufgabe4_1810653115.java
@@ -0,0 +1,23 @@
+public class Aufgabe4_1810653115
+{
+    public static void main(String[] args)
+    {
+        //Erstellen von Variablen vom Typ Integer
+        int zahl1 = 5;
+        int zahl2 = 2;
+
+        /* kleiner, größer, kleiner oder gleich, größer oder gleich
+            gleich, ungleich */
+        System.out.println(zahl1 < zahl2);
+        System.out.println(zahl2 < zahl1);
+        System.out.println(zahl1 > zahl2);
+        System.out.println(zahl2 > zahl1);
+        System.out.println(zahl1 <= zahl2);
+        System.out.println(zahl2 <= zahl1);
+        System.out.println(zahl1 >= zahl2);
+        System.out.println(zahl2 >= zahl1);
+        System.out.println(zahl1 == zahl2);
+        System.out.println(zahl1 != zahl2);
+
+    }
+}
